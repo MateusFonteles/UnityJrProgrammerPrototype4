ba6cbe7ae1ba1c819f0f2a0e5efe9d69b5d8b19d
MateusFonteles <hello.mateusfonteles@gmail.com>
2023-01-12 22:40:27 -0300

    First Commit
    Base done, missing challenge and Bonus

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private int spawnRange = 9;
+    private int enemyCount;
+    private int waveCount = 1;
+    public GameObject powerupPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnEnemyWave(waveCount);
+        Instantiate(powerupPrefab, generateSpawnPosition(), powerupPrefab.transform.rotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //What we are looking for is the scripts called Enemy,
+        //1 for each spawned enemy on scene
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveCount++;
+            spawnEnemyWave(waveCount);
+            Instantiate(powerupPrefab, generateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+    void spawnEnemyWave (int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn;  i++)
+        {
+        Instantiate(enemyPrefab, generateSpawnPosition(), 
+        enemyPrefab.transform.rotation);
+        }
+
+    }
+    private Vector3 generateSpawnPosition()
+    {
+        int randomX = Random.Range(-spawnRange, spawnRange);
+        int randomY = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPosition = new Vector3(randomX,0,randomY);
+        return randomPosition;
+    }
+}
---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    public float distanceToCover = 0.1f; 
+    public float speed = 4;
+    public Vector3 startingPosition;
+    public float rotationSpeed = 2; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = startingPosition;
+        movement.y += distanceToCover * Mathf.Sin(Time.time * speed);
+        transform.position = movement; 
+        transform.Rotate ( Vector3.up * ( rotationSpeed * Time.deltaTime ) );
+    }
+
+}
